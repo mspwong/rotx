51089a40da2bcfa0bc84cecbe6fb6ad1a4c02600
Mary Wong <mspwong@hotmail.com>
2013-08-15 01:16:11 -0700

    Refactoring

--- lib/object.rb
+++ lib/object.rb
@@ -1,19 +1,39 @@
 class Object
+  attr_reader :operator, :rotation_num
+
   def rotx(rotation_num, string, cipher=:encrypt)
     raise ArgumentError if ![:encrypt, :decrypt].include? cipher
-    operator = cipher == :encrypt ? '+' : '-'
+    @operator = cipher == :encrypt ? '+' : '-'
 
-    lower ||= ('a'..'z').to_a
-    upper ||= ('A'..'Z').to_a
+    raise ArgumentError if rotation_num.to_i == 0
+    @rotation_num = rotation_num
 
     string.chars.map do |char|
-      if !lower.index(char).nil?
-        lower[(lower.index(char).send(operator, rotation_num))%26]
-      elsif !upper.index(char).nil?
-        upper[(upper.index(char).send(operator, rotation_num))%26]
+      rotate char
+    end.join
+  end
+
+  private
+
+  def lower
+    @_lower ||= ('a'..'z').to_a
+  end
+
+  def upper
+    @_upper ||= ('A'..'Z').to_a
+  end
+
+  def rotate(char)
+    index = lower.index(char)
+    if !!index
+      lower[(index.send(operator, rotation_num))%26]
+    else
+      index = upper.index(char)
+      if !!index
+        upper[(index.send(operator, rotation_num))%26]
       else
         char
       end
-    end.join
+    end
   end
 end
