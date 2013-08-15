072ba7812ea8ebde00700581bffbc21df72876bc
Mary Wong <mspwong@hotmail.com>
2013-08-15 00:29:24 -0700

    Initial implementation.  Monkey patching Object not yet working with rspec.

---
+++ lib/object.rb
@@ -0,0 +1,19 @@
+class Object
+  def rotx(rotation_num, string, cipher=:encrypt)
+    raise ArgumentError if ![:encrypt, :decrypt].include? cipher
+    operator = cipher == :encrypt ? '+' : '-'
+
+    lower ||= ('a'..'z').to_a
+    upper ||= ('A'..'Z').to_a
+
+    string.chars.map do |char|
+      if lower.index(char).present?
+        lower[(lower.index(char).send(operator, rotation_num))%26]
+      elsif upper.index(char).present?
+        upper[(upper.index(char).send(operator, rotation_num))%26]
+      else
+        char
+      end
+    end.join
+  end
+end
---
+++ spec/object_spec.rb
@@ -0,0 +1,23 @@
+require 'object'
+
+describe '#rotx' do
+  it "'Hello, World' ==> 'Rovvy, Gybvn'" do
+    result = rotx 10, 'Hello, World'
+    result.should == 'Rovvy, Gybvn'
+  end
+
+  it "'Rovvy, Gybvn' ==> 'Hello, World'" do
+    result = rotx 10, 'Rovvy, Gybvn', :decrypt
+    result.should == 'Hello, World'
+  end
+
+  it "'Hello, World' ==> 'Rovvy, Gybvn'" do
+    result = rotx 36, 'Hello, World'
+    result.should == 'Rovvy, Gybvn'
+  end
+
+  it "raises argument error if cipher is invalid"
+
+  it "raises argument error if string contains non-alpha characters"
+
+end
